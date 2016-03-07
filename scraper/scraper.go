d9357986db6d18571cf79447967150802ebd607e
Todd Metheny <todd.metheny@gmail.com>
2016-03-07 07:40:40 -0500

    trying to make this scraper work but getting killed

--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,10 +1,51 @@
+//http://schier.co/blog/2015/04/26/a-simple-web-scraper-in-go.html
+
+package main
+
+import (
+  "net/http"
+
+  "golang.org/x/net/html"
+
+  "fmt"
+)
+
+var url = "https://clutchprep.com"
+
 //~~~~~~~~~~~~~~~~~~~~~~//
 // Make an HTTP request //
 //~~~~~~~~~~~~~~~~~~~~~~//
 
-resp, _ := http.Get(url)
-bytes, _ := ioutil.ReadAll(resp.Body)
+func httpRequest(url string){
+  resp, _ := http.Get(url)
+  bytes, _ := ioutil.ReadAll(resp.Body)
 
-fmt.Println("HTML:\n\n", string(bytes))
+  fmt.Println("HTML:\n\n", string(bytes))
 
-resp.Body.Close()
+  resp.Body.Close()
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+// Parse HTML for Anchor Tags //
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~//
+
+func parseHtml(){
+  z := html.NewTokenizer(response.Body)
+
+  for {
+    tt := z.Next()
+
+    switch {
+    case tt == html.ErrorToken:
+        // End of the document, we're done
+        return
+    case tt == html.StartTagToken:
+        t := z.Token()
+
+        isAnchor := t.Data == "a"
+        if isAnchor {
+            fmt.Println("We found a link!")
+        }
+    }
+  }
+}
